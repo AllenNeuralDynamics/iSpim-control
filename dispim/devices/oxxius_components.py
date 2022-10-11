b13c92209e852338b919ea7d4f8cddd333917c0b
Micah Woodard <micah.woodard@alleninstitute.org>
2022-10-11 16:10:34 -0700

    tested oxxius laser components

--- dispim/devices/oxxius_components.py
+++ dispim/devices/oxxius_components.py
@@ -2,10 +2,11 @@ from oxxius_laser import OxxiusLaser
 from serial import Serial
 
 class LaserHub(OxxiusLaser):
-    def __int__(self, prefix: str, serial: Serial):
-        super.__init__(serial)
+    def __init__(self, prefix: str, serial: Serial):
+        super().__init__(self, serial)
         self.prefix = prefix
 
-    def send(self, msg):
-        super._send(f"{self.prefix} {msg}")
+    def _send(self, msg):
+        prefix_msg = f'{self.prefix} {msg}'
+        return super()._send(prefix_msg)
 
